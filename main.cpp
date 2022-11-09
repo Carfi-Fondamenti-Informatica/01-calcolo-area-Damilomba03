441278eae5247e258c4ef82d759e074d22e54ae5
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-11-09 21:39:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,5 +9,5 @@ cin >> a >> b >> c;
 cout<<"AREA TRIANGOLO="<<((a*b)/2)<<endl;
 cout<<"AREA QUADRATO="<<(pow(a,2))<<endl;
 cout<<"AREA RETTANGOLO="<<(a*b)<<endl;
-cout<<"AERA TRAPEZIO="<<((a+b)*c)/2<<endl;
+cout<<"AERA TRAPEZIO="<<(((a+b)*c)/2)<<endl;
 }
