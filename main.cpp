4c87227654c81a9ef74875b2f8bfc546ae00680c
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-11-10 22:56:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main(){
 float a=0,b=0,c=0;
 cin >> a >> b >> c;
-cout<<"AREA TRIANGOLO="<<((a*b)/2)<<endl;
-cout<<"AREA QUADRATO="<<(pow(a,2))<<endl;
-cout<<"AREA RETTANGOLO="<<(a*b)<<endl;
-cout<<"AERA TRAPEZIO="<<(((a+b)*c)/2)<<endl;
+cout<<((a*b)/2)<<endl;
+cout<<(a*a)<<endl;
+cout<<(a*b)<<endl;
+cout<<(((a+b)*c)/2)<<endl;
 }
